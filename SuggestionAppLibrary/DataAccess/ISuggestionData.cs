6b9972ae2f33e9479671a79adcc5f1742cf9774f
Phillip Darby <darby.phillip@gmail.com>
2022-04-10 11:53:36 -0400

    Completed Suggestion Data access

---
+++ SuggestionAppLibrary/DataAccess/ISuggestionData.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SuggestionAppLibrary.DataAccess;
+
+public interface ISuggestionData
+{
+   Task CreateSuggestion(SuggestionModel suggestion);
+   Task<List<SuggestionModel>> GetAllApprovedSuggestionsAsync();
+   Task<List<SuggestionModel>> GetAllSuggestionsAsync();
+   Task<List<SuggestionModel>> GetAllSuggestionsWaitingForApprovalAsync();
+   Task<SuggestionModel> GetSuggestionByIdAsync(string id);
+   Task UpdateSuggestion(SuggestionModel suggestion);
+   Task UpvoteSuggestion(string suggestionId, string userId);
+}
